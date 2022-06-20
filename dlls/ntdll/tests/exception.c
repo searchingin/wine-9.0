0f5837d69d43245f8c491c2741badc97377ef945
Jinoh Kang <jinoh.kang.kr@gmail.com>
2022-06-20 20:24:38 +0900

    ntdll: Filter EFlags from user-supplied CONTEXT on x86 and x64.
    Signed-off-by: Jinoh Kang <jinoh.kang.kr@gmail.com>

--- dlls/ntdll/tests/exception.c
+++ dlls/ntdll/tests/exception.c
@@ -11648,7 +11648,6 @@ static void test_eflags_sanitization(void)
         ok(status == STATUS_SUCCESS, "NtSetContextThread failed with %lx\n", status);
         status = pNtGetContextThread(thread, &context);
         ok(status == STATUS_SUCCESS, "NtGetContextThread failed with %lx\n", status);
-        todo_wine
         ok(context.EFlags == min_eflags, "Expected EFlags to be %#lx, got %#lx\n", min_eflags, context.EFlags);
 
         context.EFlags = -1;
@@ -11656,7 +11655,6 @@ static void test_eflags_sanitization(void)
         ok(status == STATUS_SUCCESS, "NtSetContextThread failed with %lx\n", status);
         status = pNtGetContextThread(thread, &context);
         ok(status == STATUS_SUCCESS, "NtGetContextThread failed with %lx\n", status);
-        todo_wine
         ok(context.EFlags == max_eflags || broken(context.EFlags == max_eflags_broken),
            "Expected EFlags to be %#lx, got %#lx\n", max_eflags, context.EFlags);
 
