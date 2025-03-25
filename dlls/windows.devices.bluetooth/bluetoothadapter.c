7a0beaeb6554fa928b21e6f267aec62fc42e6a09
Vibhav Pant <vibhavp@gmail.com>
2025-03-25 20:47:25 +0530

    windows.devices.bluetooth: Implement BluetoothAdapter::IsAdvertisementOffloadSupported.

--- dlls/windows.devices.bluetooth/bluetoothadapter.c
+++ dlls/windows.devices.bluetooth/bluetoothadapter.c
@@ -373,8 +373,16 @@ static HRESULT WINAPI bluetoothadapter_get_IsCentralRoleSupported( IBluetoothAda
 
 static HRESULT WINAPI bluetoothadapter_get_IsAdvertisementOffloadSupported( IBluetoothAdapter *iface, boolean *value )
 {
-    FIXME( "iface %p, value %p stub!\n", iface, value );
-    return E_NOTIMPL;
+    struct winebth_radio_le_info_params info = {0};
+    HRESULT hr;
+
+    TRACE( "iface %p, value %p\n", iface, value );
+
+    *value = FALSE;
+    if (FAILED((hr = bluetoothadapter_get_le_info( iface, &info ))))
+        return hr;
+    *value = !!info.adv_offload;
+    return S_OK;
 }
 
 static HRESULT WINAPI bluetoothadapter_GetRadioAsync( IBluetoothAdapter *iface, IAsyncOperation_Radio **op )
