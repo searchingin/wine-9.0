b6d8e33359444f06968fa5fe443b70c1b975c7f5
Dmitry Timoshkov <dmitry@baikal.ru>
2025-04-07 13:43:58 +0300

    win32u: Use correct helper for converting width to device units.
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>

--- dlls/win32u/font.c
+++ dlls/win32u/font.c
@@ -262,6 +262,16 @@ static inline INT height_to_LP( DC *dc, INT height )
     return GDI_ROUND( (double)height * fabs( dc->xformVport2World.eM22 ));
 }
 
+static inline INT INTERNAL_XWSTODS(DC *dc, INT width)
+{
+    POINT pt[2];
+    pt[0].x = pt[0].y = 0;
+    pt[1].x = width;
+    pt[1].y = 0;
+    lp_to_dp(dc, pt, 2);
+    return pt[1].x - pt[0].x;
+}
+
 static inline INT INTERNAL_YWSTODS(DC *dc, INT height)
 {
     POINT pt[2];
@@ -5758,7 +5768,7 @@ BOOL nulldrv_ExtTextOut( PHYSDEV dev, INT x, INT y, UINT flags, const RECT *rect
  */
 static inline int get_line_width( DC *dc, int metric_size )
 {
-    int width = abs( INTERNAL_YWSTODS( dc, metric_size ));
+    int width = abs( INTERNAL_XWSTODS( dc, metric_size ));
     if (width == 0) width = 1;
     if (metric_size < 0) width = -width;
     return width;
