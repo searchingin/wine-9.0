f5c5866c135d14e9a8f8fd2b321abb43e4287326
Yuxuan Shui <yshui@codeweavers.com>
2025-05-13 11:34:07 +0100

    ntdll: Implement reporting of ASan errors.

--- dlls/ntdll/asan_load_store.h
+++ dlls/ntdll/asan_load_store.h
@@ -3,6 +3,73 @@
  * be the same anyways. */
 #define _RET_IP_ __builtin_return_address(0)
 
+#define ASAN_SHADOW_TO_MEM(shadow)                                                                 \
+    ((ULONG_PTR)(shadow - ASAN_SHADOW_OFFSET) << ASAN_SHADOW_SCALE_SHIFT)
+
+static BOOL NOBUILTIN asan_report(const void *addr, SIZE_T size, BOOL is_write, void *ip)
+{
+    UINT8 *shadow = (UINT8 *)ASAN_MEM_TO_SHADOW(addr), *stack_start, *stack_end;
+    INT8 *probe = (INT8 *)shadow;
+    ERR("ASan: %s of %llu bytes at %p, caller %p\n", is_write ? "write" : "read",
+            (unsigned long long)size, addr, ip);
+    if (!*shadow)
+    {
+        SIZE_T offset = 0;
+        if (size <= 8) return TRUE; /* huh?? */
+        while (!*shadow)
+        {
+            offset += ASAN_GRANULE_SIZE;
+            shadow++;
+        }
+        if (offset >= size)
+        {
+            ERR("\tno poisoned bytes\n");
+            return TRUE; /* huh??? */
+        }
+        ERR("\tfirst poisoned byte is %p+%llu = %p\n", addr, (unsigned long long)offset,
+                (void *)((ULONG_PTR)addr + offset));
+        probe = (INT8 *)shadow;
+        addr = (const INT8 *)addr + offset;
+    }
+    if (*shadow < 0x80)
+    {
+        ERR("\tpartial granule: %u\n", *shadow);
+        shadow++;
+        probe++;
+    }
+    switch (*shadow)
+    {
+    case ASAN_STACK_LEFT_REDZONE_MAGIC:
+        while (*probe < 0) probe++;
+        ERR("\tstack-buffer-underflow, addr is %lu bytes to the left of the start of stack\n",
+                (unsigned long)(ASAN_SHADOW_TO_MEM(probe) - (ULONG_PTR)addr));
+        break;
+    case ASAN_STACK_MID_REDZONE_MAGIC:
+    case ASAN_STACK_RIGHT_REDZONE_MAGIC:
+        while (*probe < 0) probe--;
+        ERR("\tstack-buffer-overflow, addr is %lu bytes to the right of %s\n",
+                (unsigned long)((ULONG_PTR)addr - ASAN_SHADOW_TO_MEM(probe)),
+                *shadow == ASAN_STACK_MID_REDZONE_MAGIC ? "another stack variable" : "the end of stack");
+        break;
+    default: ERR("\tshadow: %02x\n", *shadow); break;
+    }
+
+    switch (*shadow)
+    {
+    case ASAN_STACK_LEFT_REDZONE_MAGIC:
+    case ASAN_STACK_MID_REDZONE_MAGIC:
+    case ASAN_STACK_RIGHT_REDZONE_MAGIC:
+        stack_start = stack_end = (UINT8 *)probe;
+        while (*stack_start != ASAN_STACK_LEFT_REDZONE_MAGIC) stack_start--;
+        while (*stack_end != ASAN_STACK_RIGHT_REDZONE_MAGIC) stack_end++;
+        stack_start++;
+        ERR("\tstack: [%p, %p)\n", (void *)ASAN_SHADOW_TO_MEM(stack_start), (void *)ASAN_SHADOW_TO_MEM(stack_end));
+        break;
+    default: break;
+    }
+    return TRUE;
+}
+
 static FORCEINLINE BOOL memory_is_poisoned_1(const void *addr)
 {
     INT8 shadow_value = *(INT8 *)ASAN_MEM_TO_SHADOW(addr);
@@ -16,12 +83,130 @@ static FORCEINLINE BOOL memory_is_poisoned_1(const void *addr)
     return FALSE;
 }
 
+static FORCEINLINE BOOL memory_is_poisoned_2_4_8(const void *addr, SIZE_T size)
+{
+    UINT8 *shadow_addr = (UINT8 *)ASAN_MEM_TO_SHADOW(addr);
+
+    /*
+     * Access crosses 8(shadow size)-byte boundary. Such access maps
+     * into 2 shadow bytes, so we need to check them both.
+     */
+    if ((((ULONG_PTR)addr + size - 1) & ASAN_GRANULE_MASK) < size - 1)
+        return *shadow_addr || memory_is_poisoned_1((const INT8 *)addr + size - 1);
+
+    return memory_is_poisoned_1((const INT8 *)addr + size - 1);
+}
+
+static FORCEINLINE BOOL memory_is_poisoned_16(const void *addr)
+{
+    UINT16 *shadow_addr = (UINT16 *)ASAN_MEM_TO_SHADOW(addr);
+
+    /* Unaligned 16-bytes access maps into 3 shadow bytes. */
+    if (((ULONG_PTR)addr) & ASAN_GRANULE_MASK)
+        return *shadow_addr || memory_is_poisoned_1((const INT8 *)addr + 15);
+
+    return *shadow_addr;
+}
+
+static FORCEINLINE ULONG_PTR bytes_is_nonzero(const UINT8 *start, SIZE_T size)
+{
+    while (size)
+    {
+        if (*start) return (ULONG_PTR)start;
+        start++;
+        size--;
+    }
+
+    return 0;
+}
+
+static FORCEINLINE ULONG_PTR memory_is_nonzero(const UINT8 *start, const UINT8 *end)
+{
+    unsigned int words;
+    ULONG_PTR ret;
+    unsigned int prefix = (unsigned int)((ULONG_PTR)start % 8);
+
+    if (end - start <= 16) return bytes_is_nonzero(start, end - start);
+
+    if (prefix)
+    {
+        prefix = 8 - prefix;
+        ret = bytes_is_nonzero(start, prefix);
+        if (ret) return ret;
+        start += prefix;
+    }
+
+    words = (unsigned int)((end - start) / 8);
+    while (words)
+    {
+        if (*(UINT64 *)start) return bytes_is_nonzero(start, 8);
+        start += 8;
+        words--;
+    }
+
+    return bytes_is_nonzero(start, (end - start) % 8);
+}
+
+static FORCEINLINE BOOL memory_is_poisoned_n(const void *addr, SIZE_T size)
+{
+    ULONG_PTR ret;
+
+    ret = memory_is_nonzero((const UINT8 *)ASAN_MEM_TO_SHADOW(addr),
+            (const UINT8 *)ASAN_MEM_TO_SHADOW((const UINT8 *)addr + size - 1) + 1);
+
+    if (ret)
+    {
+        const INT8 *last_byte = (const INT8 *)addr + size - 1;
+        INT8 *last_shadow = (INT8 *)ASAN_MEM_TO_SHADOW(last_byte);
+        INT8 last_accessible_byte = (ULONG_PTR)last_byte & ASAN_GRANULE_MASK;
+
+        if (ret != (ULONG_PTR)last_shadow || last_accessible_byte >= *last_shadow) return TRUE;
+    }
+    return FALSE;
+}
+
+static FORCEINLINE BOOL memory_is_poisoned(const void *addr, SIZE_T size)
+{
+    if (__builtin_constant_p(size))
+    {
+        switch (size)
+        {
+        case 1: return memory_is_poisoned_1(addr);
+        case 2:
+        case 4:
+        case 8: return memory_is_poisoned_2_4_8(addr, size);
+        case 16: return memory_is_poisoned_16(addr);
+        default: asan_abort();
+        }
+    }
+
+    return memory_is_poisoned_n(addr, size);
+}
+
+static FORCEINLINE BOOL check_region_inline(const char *addr, SIZE_T size, BOOL write, void *ret_ip)
+{
+    if (size == 0) return TRUE;
+
+    if ((ULONG_PTR)addr + size < (ULONG_PTR)addr) return !asan_report(addr, size, write, ret_ip);
+
+    if (!memory_is_poisoned(addr, size)) return TRUE;
+
+    return !asan_report(addr, size, write, ret_ip);
+}
+
+static FORCEINLINE void check_region_inline_abort(const char *addr, SIZE_T size, BOOL write, void *ret_ip)
+{
+    if (!check_region_inline(addr, size, write, ret_ip)) asan_abort();
+}
+
 #define DEFINE_ASAN_LOAD_STORE(size)                                                               \
     ASANAPI void __asan_load##size(void *addr)                                                     \
     {                                                                                              \
+        check_region_inline_abort(addr, size, FALSE, _RET_IP_);                                    \
     }                                                                                              \
     ASANAPI void __asan_store##size(void *addr)                                                    \
     {                                                                                              \
+        check_region_inline_abort(addr, size, TRUE, _RET_IP_);                                     \
     }
 
 DEFINE_ASAN_LOAD_STORE(1)
@@ -32,18 +217,22 @@ DEFINE_ASAN_LOAD_STORE(16)
 
 ASANAPI void __asan_loadN(void *addr, SIZE_T size)
 {
+    check_region_inline_abort(addr, size, FALSE, _RET_IP_);
 }
 
 ASANAPI void __asan_storeN(void *addr, SIZE_T size)
 {
+    check_region_inline_abort(addr, size, TRUE, _RET_IP_);
 }
 
 #define DEFINE_ASAN_LOAD_STORE_NOABORT(size)                                                       \
     ASANAPI void __asan_load##size##_noabort(void *addr)                                           \
     {                                                                                              \
+        check_region_inline(addr, size, FALSE, _RET_IP_);                                          \
     }                                                                                              \
     ASANAPI void __asan_store##size##_noabort(void *addr)                                          \
     {                                                                                              \
+        check_region_inline(addr, size, TRUE, _RET_IP_);                                           \
     }
 
 DEFINE_ASAN_LOAD_STORE_NOABORT(1)
@@ -54,10 +243,12 @@ DEFINE_ASAN_LOAD_STORE_NOABORT(16)
 
 ASANAPI void __asan_loadN_noabort(void *addr, SIZE_T size)
 {
+    check_region_inline(addr, size, FALSE, _RET_IP_);
 }
 
 ASANAPI void __asan_storeN_noabort(void *addr, SIZE_T size)
 {
+    check_region_inline(addr, size, TRUE, _RET_IP_);
 }
 
 /* Emitted by the compiler to [un]poison local variables. */
@@ -84,17 +275,21 @@ DEFINE_ASAN_SET_SHADOW(f8);
 #define DEFINE_ASAN_REPORT_LOAD_STORE(size)                                                        \
     ASANAPI void DECLSPEC_NORETURN __asan_report_load##size(void *addr)                            \
     {                                                                                              \
+        asan_report(addr, size, FALSE, _RET_IP_);                                                  \
         asan_abort();                                                                              \
     }                                                                                              \
     ASANAPI void DECLSPEC_NORETURN __asan_report_store##size(void *addr)                           \
     {                                                                                              \
+        asan_report(addr, size, TRUE, _RET_IP_);                                                   \
         asan_abort();                                                                              \
     }                                                                                              \
     ASANAPI void __asan_report_load##size##_noabort(void *addr)                                    \
     {                                                                                              \
+        asan_report(addr, size, FALSE, _RET_IP_);                                                  \
     }                                                                                              \
     ASANAPI void __asan_report_store##size##_noabort(void *addr)                                   \
     {                                                                                              \
+        asan_report(addr, size, TRUE, _RET_IP_);                                                   \
     }
 
 DEFINE_ASAN_REPORT_LOAD_STORE(1)
@@ -105,28 +300,32 @@ DEFINE_ASAN_REPORT_LOAD_STORE(16)
 
 ASANAPI void DECLSPEC_NORETURN __asan_report_load_n(void *addr, SIZE_T size)
 {
+    asan_report(addr, size, FALSE, _RET_IP_);
     asan_abort();
 }
 ASANAPI void DECLSPEC_NORETURN __asan_report_store_n(void *addr, SIZE_T size)
 {
+    asan_report(addr, size, TRUE, _RET_IP_);
     asan_abort();
 }
 ASANAPI void __asan_report_load_n_noabort(void *addr, SIZE_T size)
 {
+    asan_report(addr, size, FALSE, _RET_IP_);
 }
 ASANAPI void __asan_report_store_n_noabort(void *addr, SIZE_T size)
 {
+    asan_report(addr, size, TRUE, _RET_IP_);
 }
 
 /* Functions concerning memory load and store (experimental variants) */
 #define DEFINE_ASAN_EXP_LOAD_STORE(size)                                                           \
     ASANAPI void __asan_exp_load##size(void *addr, UINT32 exp)                                     \
     {                                                                                              \
-        __asan_load##size((void *)addr);                                                           \
+        __asan_load##size(addr);                                                                   \
     }                                                                                              \
     ASANAPI void __asan_exp_store##size(void *addr, UINT32 exp)                                    \
     {                                                                                              \
-        __asan_store##size((void *)addr);                                                          \
+        __asan_store##size(addr);                                                                  \
     }
 
 DEFINE_ASAN_EXP_LOAD_STORE(1)
@@ -137,11 +336,11 @@ DEFINE_ASAN_EXP_LOAD_STORE(16)
 
 void __asan_exp_loadN(void *addr, SIZE_T size, UINT32 exp)
 {
-    __asan_loadN((void *)addr, size);
+    __asan_loadN(addr, size);
 }
 void __asan_exp_storeN(void *addr, SIZE_T size, UINT32 exp)
 {
-    __asan_storeN((void *)addr, size);
+    __asan_storeN(addr, size);
 }
 
 /* Functions concerning memory load and store reporting (experimental variants) */
