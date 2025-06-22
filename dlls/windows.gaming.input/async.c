emtee40/wine

windows.gaming.input: Use a separate interface for IAgileObject.

dlls/windows.gaming.input/async.c
@@ -32,6 +32,7 @@ struct async_info
 {
     IWineAsyncInfoImpl IWineAsyncInfoImpl_iface;
     IAsyncInfo IAsyncInfo_iface;
+    IAgileObject IAgileObject_iface;
     IInspectable *IInspectable_outer;
     LONG ref;
 
@@ -60,7 +61,6 @@ static HRESULT WINAPI async_impl_QueryInterface( IWineAsyncInfoImpl *iface, REFI
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IWineAsyncInfoImpl ))
     {
         IInspectable_AddRef( (*out = &impl->IWineAsyncInfoImpl_iface) );
@@ -70,6 +70,12 @@ static HRESULT WINAPI async_impl_QueryInterface( IWineAsyncInfoImpl *iface, REFI
     if (IsEqualGUID( iid, &IID_IAsyncInfo ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncInfo_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -299,6 +305,8 @@ static const struct IAsyncInfoVtbl async_info_vtbl =
     async_info_Close,
 };
 
+DEFINE_IAGILEOBJECT( async_info, IInspectable, object->IInspectable_outer );
+
 static void CALLBACK async_info_callback( TP_CALLBACK_INSTANCE *instance, void *iface, TP_WORK *work )
 {
     struct async_info *impl = impl_from_IWineAsyncInfoImpl( iface );
@@ -340,6 +348,7 @@ static HRESULT async_info_create( IUnknown *invoker, IUnknown *param, async_oper
     if (!(impl = calloc( 1, sizeof(struct async_info) ))) return E_OUTOFMEMORY;
     impl->IWineAsyncInfoImpl_iface.lpVtbl = &async_impl_vtbl;
     impl->IAsyncInfo_iface.lpVtbl = &async_info_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &async_info_IAgileObject_vtbl;
     impl->IInspectable_outer = outer;
     impl->ref = 1;
 
@@ -383,7 +392,6 @@ static HRESULT WINAPI async_bool_QueryInterface( IAsyncOperation_boolean *iface,
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IAsyncOperation_boolean ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncOperation_boolean_iface) );
@@ -528,7 +536,6 @@ static HRESULT WINAPI async_result_QueryInterface( IAsyncOperation_ForceFeedback
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IAsyncOperation_ForceFeedbackLoadEffectResult ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncOperation_ForceFeedbackLoadEffectResult_iface) );

dlls/windows.gaming.input/force_feedback.c
@@ -34,6 +34,7 @@ struct effect
 {
     IWineForceFeedbackEffectImpl IWineForceFeedbackEffectImpl_iface;
     IForceFeedbackEffect IForceFeedbackEffect_iface;
+    IAgileObject IAgileObject_iface;
     IInspectable *IInspectable_outer;
     LONG ref;
 
@@ -65,7 +66,6 @@ static HRESULT WINAPI effect_impl_QueryInterface( IWineForceFeedbackEffectImpl *
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IWineForceFeedbackEffectImpl ))
     {
         IWineForceFeedbackEffectImpl_AddRef( (*out = &impl->IWineForceFeedbackEffectImpl_iface) );
@@ -75,6 +75,12 @@ static HRESULT WINAPI effect_impl_QueryInterface( IWineForceFeedbackEffectImpl *
     if (IsEqualGUID( iid, &IID_IForceFeedbackEffect ))
     {
         IInspectable_AddRef( (*out = &impl->IForceFeedbackEffect_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -344,6 +350,8 @@ static const struct IForceFeedbackEffectVtbl effect_vtbl =
     effect_Stop,
 };
 
+DEFINE_IAGILEOBJECT( effect, IInspectable, object->IInspectable_outer );
+
 HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IInspectable *outer, IWineForceFeedbackEffectImpl **out )
 {
     struct effect *impl;
@@ -353,6 +361,7 @@ HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IIn
     if (!(impl = calloc( 1, sizeof(*impl) ))) return E_OUTOFMEMORY;
     impl->IWineForceFeedbackEffectImpl_iface.lpVtbl = &effect_impl_vtbl;
     impl->IForceFeedbackEffect_iface.lpVtbl = &effect_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &effect_IAgileObject_vtbl;
     impl->IInspectable_outer = outer;
     impl->ref = 1;
 
@@ -431,6 +440,7 @@ HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IIn
 struct motor
 {
     IForceFeedbackMotor IForceFeedbackMotor_iface;
+    IAgileObject IAgileObject_iface;
     LONG ref;
 
     IDirectInputDevice8W *device;
@@ -449,10 +459,15 @@ static HRESULT WINAPI motor_QueryInterface( IForceFeedbackMotor *iface, REFIID i
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IForceFeedbackMotor ))
     {
         IInspectable_AddRef( (*out = &impl->IForceFeedbackMotor_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -810,6 +825,8 @@ static const struct IForceFeedbackMotorVtbl motor_vtbl =
     motor_TryUnloadEffectAsync,
 };
 
+DEFINE_IAGILEOBJECT( motor, IForceFeedbackMotor, &object->IForceFeedbackMotor_iface );
+
 HRESULT force_feedback_motor_create( IDirectInputDevice8W *device, IForceFeedbackMotor **out )
 {
     struct motor *impl;
@@ -823,6 +840,7 @@ HRESULT force_feedback_motor_create( IDirectInputDevice8W *device, IForceFeedbac
 
     if (!(impl = calloc( 1, sizeof(*impl) ))) return E_OUTOFMEMORY;
     impl->IForceFeedbackMotor_iface.lpVtbl = &motor_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &motor_IAgileObject_vtbl;
     impl->ref = 1;
 
     IDirectInputDevice_AddRef( device );

dlls/windows.gaming.input/private.h
@@ -122,6 +122,29 @@ extern HRESULT async_operation_effect_result_create( IUnknown *invoker, IUnknown
 #define DEFINE_IINSPECTABLE_OUTER( pfx, iface_type, impl_type, outer_iface )                       \
     DEFINE_IINSPECTABLE_( pfx, iface_type, impl_type, impl_from_##iface_type, iface_type##_iface, impl->outer_iface )
 
+#define DEFINE_IAGILEOBJECT( type, base, expr )             \
+    static HRESULT WINAPI type##_IAgileObject_QueryInterface( IAgileObject *iface, REFIID iid, void **out ) \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_QueryInterface( (expr), iid, out );                                          \
+    }                                                                                              \
+    static ULONG WINAPI type##_IAgileObject_AddRef( IAgileObject *iface )                          \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_AddRef( (expr) );                                                            \
+    }                                                                                              \
+    static ULONG WINAPI type##_IAgileObject_Release( IAgileObject *iface )                         \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_Release( (expr) );                                                           \
+    }                                                                                              \
+    static const IAgileObjectVtbl type##_IAgileObject_vtbl =                                       \
+    {                                                                                              \
+        type##_IAgileObject_QueryInterface,                                                        \
+        type##_IAgileObject_AddRef,                                                                \
+        type##_IAgileObject_Release,                                                               \
+    };
+
 static inline const char *debugstr_vector3( const Vector3 *vector )
 {
     if (!vector) return "(null)";

dlls/windows.gaming.input/provider.c
@@ -46,6 +46,7 @@ struct provider
 {
     IWineGameControllerProvider IWineGameControllerProvider_iface;
     IGameControllerProvider IGameControllerProvider_iface;
+    IAgileObject IAgileObject_iface;
     LONG ref;
 
     IDirectInputDevice8W *dinput_device;
@@ -78,7 +79,6 @@ static HRESULT WINAPI wine_provider_QueryInterface( IWineGameControllerProvider 
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IWineGameControllerProvider ))
     {
         IInspectable_AddRef( (*out = &impl->IWineGameControllerProvider_iface) );
@@ -88,6 +88,12 @@ static HRESULT WINAPI wine_provider_QueryInterface( IWineGameControllerProvider 
     if (IsEqualGUID( iid, &IID_IGameControllerProvider ))
     {
         IInspectable_AddRef( (*out = &impl->IGameControllerProvider_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -427,6 +433,8 @@ static const struct IGameControllerProviderVtbl game_provider_vtbl =
     game_provider_get_IsConnected,
 };
 
+DEFINE_IAGILEOBJECT( provider, IWineGameControllerProvider, &object->IWineGameControllerProvider_iface );
+
 static void check_haptics_caps( struct provider *provider, HANDLE device, PHIDP_PREPARSED_DATA preparsed,
                                 HIDP_LINK_COLLECTION_NODE *collections, HIDP_VALUE_CAPS *caps )
 {
@@ -573,6 +581,7 @@ void provider_create( const WCHAR *device_path )
     if (!(impl = calloc( 1, sizeof(*impl) ))) goto done;
     impl->IWineGameControllerProvider_iface.lpVtbl = &wine_provider_vtbl;
     impl->IGameControllerProvider_iface.lpVtbl = &game_provider_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &provider_IAgileObject_vtbl;
     IDirectInputDevice_AddRef( dinput_device );
     impl->dinput_device = dinput_device;
     impl->ref = 1;

searchingin/wine-9.0

dlls/windows.gaming.input/async.c
@@ -34,6 +34,7 @@ struct async_info
 {
     IAsyncInfoImpl IAsyncInfoImpl_iface;
     IAsyncInfo IAsyncInfo_iface;
+    IAgileObject IAgileObject_iface;
     IInspectable *IInspectable_outer;
     LONG ref;
 
@@ -62,7 +63,6 @@ static HRESULT WINAPI async_impl_QueryInterface( IAsyncInfoImpl *iface, REFIID i
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IAsyncInfoImpl ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncInfoImpl_iface) );
@@ -72,6 +72,12 @@ static HRESULT WINAPI async_impl_QueryInterface( IAsyncInfoImpl *iface, REFIID i
     if (IsEqualGUID( iid, &IID_IAsyncInfo ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncInfo_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -302,6 +308,8 @@ static const struct IAsyncInfoVtbl async_info_vtbl =
     async_info_Close,
 };
 
+DEFINE_IAGILEOBJECT( async_info, IInspectable, object->IInspectable_outer );
+
 static void CALLBACK async_info_callback( TP_CALLBACK_INSTANCE *instance, void *iface, TP_WORK *work )
 {
     struct async_info *impl = impl_from_IAsyncInfoImpl( iface );
@@ -343,6 +351,7 @@ static HRESULT async_info_create( IUnknown *invoker, IUnknown *param, async_oper
     if (!(impl = calloc( 1, sizeof(struct async_info) ))) return E_OUTOFMEMORY;
     impl->IAsyncInfoImpl_iface.lpVtbl = &async_impl_vtbl;
     impl->IAsyncInfo_iface.lpVtbl = &async_info_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &async_info_agile_vtbl;
     impl->IInspectable_outer = outer;
     impl->ref = 1;
 
@@ -386,7 +395,6 @@ static HRESULT WINAPI async_bool_QueryInterface( IAsyncOperation_boolean *iface,
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IAsyncOperation_boolean ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncOperation_boolean_iface) );
@@ -531,7 +539,6 @@ static HRESULT WINAPI async_result_QueryInterface( IAsyncOperation_ForceFeedback
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IAsyncOperation_ForceFeedbackLoadEffectResult ))
     {
         IInspectable_AddRef( (*out = &impl->IAsyncOperation_ForceFeedbackLoadEffectResult_iface) );

dlls/windows.gaming.input/force_feedback.c
@@ -34,6 +34,7 @@ struct effect
 {
     IWineForceFeedbackEffectImpl IWineForceFeedbackEffectImpl_iface;
     IForceFeedbackEffect IForceFeedbackEffect_iface;
+    IAgileObject IAgileObject_iface;
     IInspectable *IInspectable_outer;
     LONG ref;
 
@@ -65,7 +66,6 @@ static HRESULT WINAPI effect_impl_QueryInterface( IWineForceFeedbackEffectImpl *
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IWineForceFeedbackEffectImpl ))
     {
         IWineForceFeedbackEffectImpl_AddRef( (*out = &impl->IWineForceFeedbackEffectImpl_iface) );
@@ -75,6 +75,12 @@ static HRESULT WINAPI effect_impl_QueryInterface( IWineForceFeedbackEffectImpl *
     if (IsEqualGUID( iid, &IID_IForceFeedbackEffect ))
     {
         IInspectable_AddRef( (*out = &impl->IForceFeedbackEffect_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -344,6 +350,8 @@ static const struct IForceFeedbackEffectVtbl effect_vtbl =
     effect_Stop,
 };
 
+DEFINE_IAGILEOBJECT( effect, IInspectable, object->IInspectable_outer );
+
 HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IInspectable *outer, IWineForceFeedbackEffectImpl **out )
 {
     struct effect *impl;
@@ -353,6 +361,7 @@ HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IIn
     if (!(impl = calloc( 1, sizeof(*impl) ))) return E_OUTOFMEMORY;
     impl->IWineForceFeedbackEffectImpl_iface.lpVtbl = &effect_impl_vtbl;
     impl->IForceFeedbackEffect_iface.lpVtbl = &effect_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &effect_agile_vtbl;
     impl->IInspectable_outer = outer;
     impl->ref = 1;
 
@@ -431,6 +440,7 @@ HRESULT force_feedback_effect_create( enum WineForceFeedbackEffectType type, IIn
 struct motor
 {
     IForceFeedbackMotor IForceFeedbackMotor_iface;
+    IAgileObject IAgileObject_iface;
     LONG ref;
 
     IDirectInputDevice8W *device;
@@ -449,10 +459,15 @@ static HRESULT WINAPI motor_QueryInterface( IForceFeedbackMotor *iface, REFIID i
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IForceFeedbackMotor ))
     {
         IInspectable_AddRef( (*out = &impl->IForceFeedbackMotor_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -810,6 +825,8 @@ static const struct IForceFeedbackMotorVtbl motor_vtbl =
     motor_TryUnloadEffectAsync,
 };
 
+DEFINE_IAGILEOBJECT( motor, IForceFeedbackMotor, &object->IForceFeedbackMotor_iface );
+
 HRESULT force_feedback_motor_create( IDirectInputDevice8W *device, IForceFeedbackMotor **out )
 {
     struct motor *impl;
@@ -823,6 +840,7 @@ HRESULT force_feedback_motor_create( IDirectInputDevice8W *device, IForceFeedbac
 
     if (!(impl = calloc( 1, sizeof(*impl) ))) return E_OUTOFMEMORY;
     impl->IForceFeedbackMotor_iface.lpVtbl = &motor_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &motor_agile_vtbl;
     impl->ref = 1;
 
     IDirectInputDevice_AddRef( device );

dlls/windows.gaming.input/private.h
@@ -122,6 +122,29 @@ extern HRESULT async_operation_effect_result_create( IUnknown *invoker, IUnknown
 #define DEFINE_IINSPECTABLE_OUTER( pfx, iface_type, impl_type, outer_iface )                       \
     DEFINE_IINSPECTABLE_( pfx, iface_type, impl_type, impl_from_##iface_type, iface_type##_iface, impl->outer_iface )
 
+#define DEFINE_IAGILEOBJECT( type, base, expr )             \
+    static HRESULT WINAPI type##_agile_QueryInterface( IAgileObject *iface, REFIID iid, void **out ) \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_QueryInterface( (expr), iid, out );                                          \
+    }                                                                                              \
+    static ULONG WINAPI type##_agile_AddRef( IAgileObject *iface )                                 \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_AddRef( (expr) );                                                            \
+    }                                                                                              \
+    static ULONG WINAPI type##_agile_Release( IAgileObject *iface )                                \
+    {                                                                                              \
+        struct type *object = CONTAINING_RECORD( iface, struct type, IAgileObject_iface );         \
+        return base##_Release( (expr) );                                                           \
+    }                                                                                              \
+    static const IAgileObjectVtbl type##_agile_vtbl =                                              \
+    {                                                                                              \
+        type##_agile_QueryInterface,                                                               \
+        type##_agile_AddRef,                                                                       \
+        type##_agile_Release,                                                                      \
+    };
+
 static inline const char *debugstr_vector3( const Vector3 *vector )
 {
     if (!vector) return "(null)";

dlls/windows.gaming.input/provider.c
@@ -46,6 +46,7 @@ struct provider
 {
     IWineGameControllerProvider IWineGameControllerProvider_iface;
     IGameControllerProvider IGameControllerProvider_iface;
+    IAgileObject IAgileObject_iface;
     LONG ref;
 
     IDirectInputDevice8W *dinput_device;
@@ -78,7 +79,6 @@ static HRESULT WINAPI wine_provider_QueryInterface( IWineGameControllerProvider 
 
     if (IsEqualGUID( iid, &IID_IUnknown ) ||
         IsEqualGUID( iid, &IID_IInspectable ) ||
-        IsEqualGUID( iid, &IID_IAgileObject ) ||
         IsEqualGUID( iid, &IID_IWineGameControllerProvider ))
     {
         IInspectable_AddRef( (*out = &impl->IWineGameControllerProvider_iface) );
@@ -88,6 +88,12 @@ static HRESULT WINAPI wine_provider_QueryInterface( IWineGameControllerProvider 
     if (IsEqualGUID( iid, &IID_IGameControllerProvider ))
     {
         IInspectable_AddRef( (*out = &impl->IGameControllerProvider_iface) );
+        return S_OK;
+    }
+
+    if (IsEqualGUID( iid, &IID_IAgileObject ))
+    {
+        IInspectable_AddRef( (*out = &impl->IAgileObject_iface) );
         return S_OK;
     }
 
@@ -427,6 +433,8 @@ static const struct IGameControllerProviderVtbl game_provider_vtbl =
     game_provider_get_IsConnected,
 };
 
+DEFINE_IAGILEOBJECT( provider, IWineGameControllerProvider, &object->IWineGameControllerProvider_iface );
+
 static void check_haptics_caps( struct provider *provider, HANDLE device, PHIDP_PREPARSED_DATA preparsed,
                                 HIDP_LINK_COLLECTION_NODE *collections, HIDP_VALUE_CAPS *caps )
 {
@@ -573,6 +581,7 @@ void provider_create( const WCHAR *device_path )
     if (!(impl = calloc( 1, sizeof(*impl) ))) goto done;
     impl->IWineGameControllerProvider_iface.lpVtbl = &wine_provider_vtbl;
     impl->IGameControllerProvider_iface.lpVtbl = &game_provider_vtbl;
+    impl->IAgileObject_iface.lpVtbl = &provider_agile_vtbl;
     IDirectInputDevice_AddRef( dinput_device );
     impl->dinput_device = dinput_device;
     impl->ref = 1;